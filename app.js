2a413cbec8818f1f34b9c64445906978b815dabc
Samantha Kellow <sammii.h@icloud.com>
2019-07-03 20:54:21 +0100

    working on navbar mobile styling

--- app.js
+++ app.js
@@ -23,6 +23,41 @@ $carousel.on( 'scroll.flickity', function() {
 })
 
 
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target
+        const $target = document.getElementById(target)
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+
+      })
+    })
+  }
+
+  // {`navbar-menu${this.state.active ? ' is-active' : ''}`}
+
+  // const navBurger = document.getElementById('nav-burger')
+  //
+  // navBurger.addEventListener('click', () => {
+  //   return navBurger.className === '' ? navBurger.className = ' is-active' : ''
+  // })
+
+})
+
+
 //
 // const elem = document.querySelector('.main-carousel')
 //
