3ec56ef11389008532e8a69b9241bd76c041005b
Samantha Kellow <sammii.h@icloud.com>
2021-12-13 16:29:50 +0000

    creating fully responsive UI

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log("JS loaded 🐛");
-  createInfoPanel()
-
-  projects.map(project => {
-    createImage(project);
-  });
-
-  activeProjectInfo(projects[0].id);
   
-  document.querySelectorAll(".project-image-container").forEach(item => {
-    item.addEventListener('mouseenter', event => activeProjectInfo(event.target.id) )
+  initializeInterface()
+  
+  document.querySelectorAll(".project-image-container-desktop").forEach(item => {
+    item.addEventListener('mouseenter', event => activeProjectInfo(event.target.id, 'desktop') )
   });
 });
 
@@ -61,48 +56,121 @@ const projects = [
   },
 ];
 
-function createImage(project) {
+function initializeInterface() {
+  // create desktop + mobile views
+  initializeMediaSpecificInterface('desktop')
+  initializeMediaSpecificInterface('mobile')
+}
+
+function initializeMediaSpecificInterface(mediaType) {
+  // create main containers
+  mediaType === 'mobile' ? projectsContainerMobile() : projectsContainerDesktop();
+  // create info panel instance
+  createInfoPanel(mediaType)
+  // create thumbnails
+  projects.map(project => {
+    createThumbnail(project, mediaType);
+  });
+  // set base active project on page load
+  const firstProjectId = projects[0].id
+  activeProjectInfo(`${firstProjectId}-${mediaType}`, mediaType);
+};
+
+function createThumbnail(project, mediaType) {
+  // create image
   const image = document.createElement("img")
   image.setAttribute("src", `./src/assets/images/${project.id}.png`)
-  const figure = document.createElement("figure")//.classList.add("image", "is-128x128")
+  // create figure container for image
+  const figure = document.createElement("figure")
   figure.classList.add("image", "is-1x1", "is-clickable")
+  // create github hyperlink
   const link = document.createElement("a")
   link.setAttribute("href", `https://github.com/Sammii-HK/${project.id}`)
   link.setAttribute("target", "_blank")
+  // mediaType dependant column
   const column = document.createElement("div")
-  column.classList.add("column", "is-4", "project-image-container")
+  column.classList.add("column", `project-image-container-${mediaType}`)
+  column.classList.add(mediaType === 'mobile' ? 'is-12' : 'is-4')
+  column.setAttribute("id", `${project.id}-${mediaType}`)
+  // append image > figure > link > column
   figure.appendChild(image)
   link.appendChild(figure)
   column.appendChild(link)
-  column.setAttribute("id", project.id)
-  const projectsContainer = document.getElementById("projects")
+  // append column > projects container
+  const projectsContainer = document.getElementById(`${mediaType}-projects`)
   return projectsContainer.appendChild(column)
 };
 
-function createInfoPanel() {
+function createInfoPanel(mediaType) {
+  // create title
   const title = document.createElement("h3")
-  title.setAttribute("id", "project-title")
+  title.setAttribute("id", `project-title-${mediaType}`)
+  // create info paragraph
   const info = document.createElement("p")
-  info.setAttribute("id", "project-info")
-  
-  const projectInfo = document.getElementById("project-info-container")
+  info.setAttribute("id", `project-info-${mediaType}`)
+  // append title + info > project info container
+  const projectInfo = document.getElementById(`project-info-container-${mediaType}`)
   projectInfo.appendChild(title)
   projectInfo.appendChild(info)
 };
 
-function activeProjectInfo(projectId) {
-  const project = projects.find(proj => proj.id === projectId );
-  const title = document.getElementById("project-title");
+function activeProjectInfo(projectId, mediaType) {
+  // find project with matching id
+  const project = projects.find(proj => {
+    // append mediaType to `const projects: { id }` for reference to work
+    const mediaTypeProjId = `${proj.id}-${mediaType}`
+    // then match ids to grab project info
+    return mediaTypeProjId === projectId
+  } );
+  // find title + append `project.title`
+  const title = document.getElementById(`project-title-${mediaType}`);
   title.innerText = project.title
-  const info = document.getElementById("project-info");
+  // find info + append `project.info`
+  const info = document.getElementById(`project-info-${mediaType}`);
   info.innerText = project.info
 };
 
-// $(function(){
-//   projects.map(project => {
-//     // console.log("project.title", project.title);
-//     console.log("`#${project.id}`", `#${project.id}`);
-    
-//     $( `#${project.id}` ).hover( activeProjectInfo(project) );
-//   })
-// })
+function projectsContainerDesktop() {
+  // create + style project info container
+  const infoContainer = document.createElement("div")
+  infoContainer.classList.add("column", "is-3")
+  infoContainer.setAttribute("id", "project-info-container-desktop")
+  // create + style project thumbnails
+  const thumbnails = document.createElement("div")
+  thumbnails.classList.add("columns", "is-multiline")
+  thumbnails.setAttribute("id", "desktop-projects")
+  const thumbnailsContainer = document.createElement("div")
+  thumbnailsContainer.classList.add("column", "is-7")
+  thumbnailsContainer.appendChild(thumbnails)
+  // create + style columns container
+  const columns = document.createElement("div")
+  columns.classList.add("columns", "is-centered")
+  // append info container + thumbnails container > columns
+  columns.appendChild(infoContainer)
+  columns.appendChild(thumbnailsContainer)
+  // append columns > container
+  const container = document.getElementById("projects-container-desktop")
+  container.appendChild(columns)
+};
+
+function projectsContainerMobile() {
+  // create + style project info container
+  const infoContainer = document.createElement("div")
+  infoContainer.classList.add("column", "is-6")
+  infoContainer.setAttribute("id", "project-info-container-mobile")
+  // create + style project thumbnails
+  const thumbnailsContainer = document.createElement("div")
+  thumbnailsContainer.classList.add("column", "is-5")
+  thumbnailsContainer.setAttribute("id", "mobile-projects")
+  // create + style columns container
+  const columns = document.createElement("div")
+  columns.classList.add("columns", "is-centered", "is-mobile")
+  // append info container + thumbnails container > columns
+  columns.appendChild(infoContainer)
+  columns.appendChild(thumbnailsContainer)
+  // append columns > container
+  const container = document.getElementById("projects-container-mobile")
+  container.appendChild(columns)
+};
+
+// $(function(){})
