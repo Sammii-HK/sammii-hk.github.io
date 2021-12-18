f3a39372c6367669b3b28a147126596de1ac3433
Sammii <sammii.h@icloud.com>
2021-12-18 17:44:40 +0000

    Feature/2.0 (#1)
    * created base UI
    
    * creating project info hover event
    
    * adding github hyperlink to project image
    
    * adding new images, deleting old images
    
    * creating fully responsive UI
    
    * mobile scroll event on carousel for active project
    
    * refactoring + final UI touches
    
    * adding techStack property to projects
    
    * thumbnail overlays which toggle on activeProject
    
    * populating app projects info data
    
    * moving create Projects functions to own file
    
    * programatically created contact detail items
    
    * creating prefers-color-scheme css
    
    * adding a site favicon
    
    * completing project info details

---
+++ modules/components/contact-info.js
@@ -0,0 +1,41 @@
+const contactOptions = [
+  {
+    link: "mailto:kellow.sammii@gmail.com",
+    icon: "envelope",
+    iconType: "fas",
+  },
+  {
+    link: "https://github.com/Sammii-HK/",
+    icon: "github",
+    iconType: "fab",
+  },
+  {
+    link: "https://www.linkedin.com/in/samantha-kellow/",
+    icon: "linkedin-in",
+    iconType: "fab",
+  },
+];
+
+function createContactLink(option) {
+  const icon = document.createElement("i")
+  icon.classList.add(option.iconType, `fa-${option.icon}`, "fa-lg")
+  const span = document.createElement("span")
+  span.classList.add("icon", "is-medium")
+  span.appendChild(icon)
+  const anchor = document.createElement("a")
+  anchor.classList.add("has-text-centered", "icon-link")
+  anchor.hasAttribute("href", option.link)
+  anchor.appendChild(span)
+  const container = document.createElement("div")
+  return container.appendChild(anchor)
+};
+
+export function contactDetails() {
+  const contactContainer = document.createElement("div")
+  contactContainer.classList.add("contact-details-container")
+  contactOptions.map(option => {
+    const contactLinks = createContactLink(option)
+    contactContainer.appendChild(contactLinks)
+  });
+  return contactContainer; 
+};
