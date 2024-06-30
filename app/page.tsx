be34eca7da843a0e452d6576be5406ea70544747
Samantha Kellow <sammii.h@icloud.com>
2024-06-30 15:17:35 +0100

    creating sammii logo svg mask with stacked radial gradients

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import { ProjectView } from "./src/components/ProjectView";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav className="nav">Sammii</nav>
+    <main className="flex flex-col items-center relative overflow-hidden">
+      <nav className="absolute bg-black z-10 w-full flex justify-center p-4">
+        <div className="pt-3 logo stacked-radial" aria-label="logo svg" />
+      </nav>
       <ProjectView />
     </main>
   );
