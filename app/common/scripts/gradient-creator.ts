2815f511d383bfa242432d0ecf6d8e53c78fb016
Samantha Kellow <sammii.h@icloud.com>
2024-07-02 20:03:47 +0100

    refactor gradient creator to own file

---
+++ app/common/scripts/gradient-creator.ts
@@ -0,0 +1,29 @@
+export const gradientCreator = (xPc: number, yPc: number) => {
+  const colourCreator = (number: number) => {
+    const colour = Math.floor((255 / 100) * number)
+    return colour < 255 ? Math.floor((255 / 100) * number) : 255;
+  };
+  
+  const colour1 = colourCreator(xPc);
+  const colour2 = colourCreator(yPc);
+  const colour3 = 255 - colourCreator(xPc);
+  
+  return {
+    background:
+      `radial-gradient(
+        circle at 50% 0,
+        rgb(${colour1} ${colour3} ${colour2} / 50%),
+        rgb(${colour1} ${colour3} ${colour2} / 0%) 70.71%
+      ),
+      radial-gradient(
+        circle at 6.7% 75%,
+        rgb(${colour3} ${colour2} ${colour1} / 50%),
+        rgb(${colour3} ${colour2} ${colour1} / 0%) 70.71%
+      ),
+      radial-gradient(
+        circle at 93.3% 75%,
+        rgb(${colour2} ${colour1} ${colour3} / 50%),
+        rgb(${colour2} ${colour1} ${colour3} / 0%) 70.71%
+      ) white`
+  };
+}
