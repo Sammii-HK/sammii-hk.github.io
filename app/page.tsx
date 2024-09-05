3af767aa0a1689a957b27cc6c8e8c0ecb0118e22
Sammii <kellow.sammii@gmail.com>
2024-09-05 19:25:28 +0100

    amending test colour

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { PortfolioContainer } from "./src/components/PortfolioContainer";
 
 export default function Home() {  
   return (
-    <main className="flex flex-col items-center relative overflow-hidden bg-black">
+    <main className="flex flex-col items-center relative overflow-hidden bg-black text-white">
       <PortfolioContainer />
     </main>
   );
