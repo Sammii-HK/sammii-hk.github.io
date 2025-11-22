90c16ace41c7afa9ece39edd9d50b76731e69cbd
Sammii <kellow.sammii@gmail.com>
2025-11-22 22:08:33 +0000

    navbar styling amends

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,13 +1,9 @@
 import { gradientCreator } from "../../common/scripts/gradient-creator";
 
-export const Navbar = ({ xPc, yPc }: { xPc: number; yPc: number }) => {
+export const Navbar = ({xPc, yPc}: {xPc: number, yPc: number}) => {
   return (
-    <nav className="absolute bg-white/80 dark:bg-black z-10 w-full flex justify-center p-4 backdrop-blur-sm">
-      <div
-        className="pt-3 logo"
-        style={gradientCreator(xPc, yPc)}
-        aria-label="logo svg"
-      />
+    <nav className="absolute top-0 left-0 right-0 bg-white/80 dark:bg-black z-10 w-full flex justify-center p-4 backdrop-blur-sm">
+      <div className="pt-3 logo" style={gradientCreator(xPc, yPc)} aria-label="logo svg" />
     </nav>
-  );
+  )
 };
